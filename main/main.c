9e568b93de708d864e0affaf9318099eac024d2a
piyush-saurabh <saurabh.piyush2807@gmail.com>
2021-06-13 13:03:28 +0530

    updated the main

--- main/main.c
+++ main/main.c
@@ -1,14 +1,26 @@
-#include<stdio.h>
+#include <stdio.h>
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
+#include <esp_log.h>
 
-void app_main(void)
+#define TAG "MAIN"
+
+// Comment it to stop debugging
+#define DEBUG
+
+void app_main()
 {
+#ifdef DEBUG
+    ESP_LOGI(TAG, "Started...");
+#endif
+
     int i = 0;
     while (true)
     {
-        i = i+1;
-        printf("Value of i = %d\n", i);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        i = i + 1;
+        ESP_LOGI(TAG, "Value of i = %d\n", i);
+
+        // Wait for 5 sec
+        vTaskDelay(5 * 1000 / portTICK_PERIOD_MS);
     }
 }
